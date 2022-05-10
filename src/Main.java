8ce87b3e882deff47c4d75be5b1d7707d417250e
kajela <titoyut56@gmail.com>
2022-05-10 06:36:56 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public Main(int y) {
+        int x;
+    }
+    int x = 10;
+    public static void main(String[] args) {
+        Main myObj = new Main(6);
+        System.out.println(myObj.x);
+    }
+}
